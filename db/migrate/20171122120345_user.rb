484d63a8e4d4bfb6352db6fe597636a680a49197
mykhaylo-ardashyn <mykhail.ardashyn@litslink.com>
2017-12-15 12:27:39 +0200

    fix migration

--- db/migrate/20171122120345_user.rb
+++ db/migrate/20171122120345_user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Migration[5.1]
   def change
-    create_table :profile do |t|
+    create_table :users do |t|
       t.string :first_name, null: false, default: ''
       t.string :last_name, null: false, default: ''
       t.string :email, null: false, default: ''
@@ -11,7 +11,7 @@ class User < ActiveRecord::Migration[5.1]
       t.timestamps
     end
 
-    add_index :profile, :email, unique: true
-    add_index :profile, :reset_password_token, unique: true
+    add_index :users, :email, unique: true
+    add_index :users, :reset_password_token, unique: true
   end
 end
